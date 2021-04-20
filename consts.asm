2f2b2d1da8a906c43cc25454ac92cabc0e8b65e1
Jasper Renow-Clarke <jrenow_clarke@yahoo.co.uk>
2021-04-20 15:50:09 +0100

    Added some contstants

---
+++ consts.asm
@@ -0,0 +1,26 @@
+PAD_RIGHT  = &01
+PAD_LEFT   = &02
+PAD_DOWN   = &04
+PAD_UP     = &08
+PAD_START  = &10
+PAD_SELECT = &20
+PAD_B      = &40
+PAD_A      = &80
+
+MAP_WIDTH = 32
+MAP_HEIGHT = 13
+
+MAP_HERE   = &00
+MAP_RIGHT  = &01
+MAP_UP     = &02
+MAP_LEFT   = &03
+MAP_DOWN   = &04
+
+MAP_EMPTY        = 0
+MAP_CONCRETE     = 1
+MAP_BRICK        = 2
+MAP_BOMB         = 3
+MAP_HIDDEN_EXIT  = 4
+MAP_HIDDEN_BONUS = 5
+MAP_BONUS        = 6
+MAP_EXIT         = 8
